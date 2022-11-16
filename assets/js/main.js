5abc13063454dc94d732badd02a3bfc7d3e2f072
Nino Lambert <anthony.lamb001@gmail.com>
2022-11-16 11:41:44 -0500

    change website layout + add table of contents. TOC needs better styling
    Co-authored-by: Eugene O'Fallon <epofallon@gmail.com>
    Co-authored-by: Aaron Hull <aahull08@gmail.com>
    Co-authored-by: Marcin Kostecki <49038365+marcinkostecki@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,155 +1,168 @@
 /**
-* Template Name: Selecao - v4.9.1
-* Template URL: https://bootstrapmade.com/selecao-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
-(function() {
+ * Template Name: Selecao - v4.9.1
+ * Template URL: https://bootstrapmade.com/selecao-bootstrap-template/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+(function () {
   "use strict";
 
   /**
    * Easy selector helper function
    */
   const select = (el, all = false) => {
-    el = el.trim()
+    el = el.trim();
     if (all) {
-      return [...document.querySelectorAll(el)]
+      return [...document.querySelectorAll(el)];
     } else {
-      return document.querySelector(el)
+      return document.querySelector(el);
     }
-  }
+  };
 
   /**
    * Easy event listener function
    */
   const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
+    let selectEl = select(el, all);
     if (selectEl) {
       if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
+        selectEl.forEach((e) => e.addEventListener(type, listener));
       } else {
-        selectEl.addEventListener(type, listener)
+        selectEl.addEventListener(type, listener);
       }
     }
-  }
+  };
 
   /**
-   * Easy on scroll event listener 
+   * Easy on scroll event listener
    */
   const onscroll = (el, listener) => {
-    el.addEventListener('scroll', listener)
-  }
+    el.addEventListener("scroll", listener);
+  };
 
   /**
    * Navbar links active state on scroll
    */
-  let navbarlinks = select('#navbar .scrollto', true)
+  let navbarlinks = select("#navbar .scrollto", true);
   const navbarlinksActive = () => {
-    let position = window.scrollY + 200
-    navbarlinks.forEach(navbarlink => {
-      if (!navbarlink.hash) return
-      let section = select(navbarlink.hash)
-      if (!section) return
-      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
-        navbarlink.classList.add('active')
+    let position = window.scrollY + 200;
+    navbarlinks.forEach((navbarlink) => {
+      if (!navbarlink.hash) return;
+      let section = select(navbarlink.hash);
+      if (!section) return;
+      if (
+        position >= section.offsetTop &&
+        position <= section.offsetTop + section.offsetHeight
+      ) {
+        navbarlink.classList.add("active");
       } else {
-        navbarlink.classList.remove('active')
+        navbarlink.classList.remove("active");
       }
-    })
-  }
-  window.addEventListener('load', navbarlinksActive)
-  onscroll(document, navbarlinksActive)
+    });
+  };
+  window.addEventListener("load", navbarlinksActive);
+  onscroll(document, navbarlinksActive);
 
   /**
    * Scrolls to an element with header offset
    */
   const scrollto = (el) => {
-    let header = select('#header')
-    let offset = header.offsetHeight
+    let header = select("#header");
+    let offset = header.offsetHeight;
 
-    let elementPos = select(el).offsetTop
+    let elementPos = select(el).offsetTop;
     window.scrollTo({
       top: elementPos - offset,
-      behavior: 'smooth'
-    })
-  }
+      behavior: "smooth",
+    });
+  };
 
   /**
    * Toggle .header-scrolled class to #header when page is scrolled
    */
-  let selectHeader = select('#header')
+  let selectHeader = select("#header");
   if (selectHeader) {
     const headerScrolled = () => {
       if (window.scrollY > 100) {
-        selectHeader.classList.add('header-scrolled')
+        selectHeader.classList.add("header-scrolled");
       } else {
-        selectHeader.classList.remove('header-scrolled')
+        selectHeader.classList.remove("header-scrolled");
       }
-    }
-    window.addEventListener('load', headerScrolled)
-    onscroll(document, headerScrolled)
+    };
+    window.addEventListener("load", headerScrolled);
+    onscroll(document, headerScrolled);
   }
 
   /**
    * Back to top button
    */
-  let backtotop = select('.back-to-top')
+  let backtotop = select(".back-to-top");
   if (backtotop) {
     const toggleBacktotop = () => {
       if (window.scrollY > 100) {
-        backtotop.classList.add('active')
+        backtotop.classList.add("active");
       } else {
-        backtotop.classList.remove('active')
+        backtotop.classList.remove("active");
       }
-    }
-    window.addEventListener('load', toggleBacktotop)
-    onscroll(document, toggleBacktotop)
+    };
+    window.addEventListener("load", toggleBacktotop);
+    onscroll(document, toggleBacktotop);
   }
 
   /**
    * Mobile nav toggle
    */
-  on('click', '.mobile-nav-toggle', function(e) {
-    select('#navbar').classList.toggle('navbar-mobile')
-    this.classList.toggle('bi-list')
-    this.classList.toggle('bi-x')
-  })
+  on("click", ".mobile-nav-toggle", function (e) {
+    select("#navbar").classList.toggle("navbar-mobile");
+    this.classList.toggle("bi-list");
+    this.classList.toggle("bi-x");
+  });
 
   /**
    * Mobile nav dropdowns activate
    */
-  on('click', '.navbar .dropdown > a', function(e) {
-    if (select('#navbar').classList.contains('navbar-mobile')) {
-      e.preventDefault()
-      this.nextElementSibling.classList.toggle('dropdown-active')
-    }
-  }, true)
+  on(
+    "click",
+    ".navbar .dropdown > a",
+    function (e) {
+      if (select("#navbar").classList.contains("navbar-mobile")) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle("dropdown-active");
+      }
+    },
+    true
+  );
 
   /**
    * Scrool with ofset on links with a class name .scrollto
    */
-  on('click', '.scrollto', function(e) {
-    if (select(this.hash)) {
-      e.preventDefault()
+  on(
+    "click",
+    ".scrollto",
+    function (e) {
+      if (select(this.hash)) {
+        e.preventDefault();
 
-      let navbar = select('#navbar')
-      if (navbar.classList.contains('navbar-mobile')) {
-        navbar.classList.remove('navbar-mobile')
-        let navbarToggle = select('.mobile-nav-toggle')
-        navbarToggle.classList.toggle('bi-list')
-        navbarToggle.classList.toggle('bi-x')
+        let navbar = select("#navbar");
+        if (navbar.classList.contains("navbar-mobile")) {
+          navbar.classList.remove("navbar-mobile");
+          let navbarToggle = select(".mobile-nav-toggle");
+          navbarToggle.classList.toggle("bi-list");
+          navbarToggle.classList.toggle("bi-x");
+        }
+        scrollto(this.hash);
       }
-      scrollto(this.hash)
-    }
-  }, true)
+    },
+    true
+  );
 
   /**
    * Scroll with ofset on page load with hash links in the url
    */
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     if (window.location.hash) {
       if (select(window.location.hash)) {
-        scrollto(window.location.hash)
+        scrollto(window.location.hash);
       }
     }
   });
@@ -157,96 +170,222 @@
   /**
    * Porfolio isotope and filter
    */
-  window.addEventListener('load', () => {
-    let portfolioContainer = select('.portfolio-container');
+  window.addEventListener("load", () => {
+    let portfolioContainer = select(".portfolio-container");
     if (portfolioContainer) {
       let portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
+        itemSelector: ".portfolio-item",
       });
 
-      let portfolioFilters = select('#portfolio-flters li', true);
+      let portfolioFilters = select("#portfolio-flters li", true);
 
-      on('click', '#portfolio-flters li', function(e) {
-        e.preventDefault();
-        portfolioFilters.forEach(function(el) {
-          el.classList.remove('filter-active');
-        });
-        this.classList.add('filter-active');
+      on(
+        "click",
+        "#portfolio-flters li",
+        function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove("filter-active");
+          });
+          this.classList.add("filter-active");
 
-        portfolioIsotope.arrange({
-          filter: this.getAttribute('data-filter')
-        });
-        portfolioIsotope.on('arrangeComplete', function() {
-          AOS.refresh()
-        });
-      }, true);
+          portfolioIsotope.arrange({
+            filter: this.getAttribute("data-filter"),
+          });
+          portfolioIsotope.on("arrangeComplete", function () {
+            AOS.refresh();
+          });
+        },
+        true
+      );
     }
-
   });
 
   /**
-   * Initiate portfolio lightbox 
+   * Initiate portfolio lightbox
    */
   const portfolioLightbox = GLightbox({
-    selector: '.portfolio-lightbox'
+    selector: ".portfolio-lightbox",
   });
 
   /**
    * Portfolio details slider
    */
-  new Swiper('.portfolio-details-slider', {
+  new Swiper(".portfolio-details-slider", {
     speed: 400,
     loop: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: false,
     },
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
-    }
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
+    },
   });
 
   /**
    * Testimonials slider
    */
-  new Swiper('.testimonials-slider', {
+  new Swiper(".testimonials-slider", {
     speed: 600,
     loop: true,
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: false,
     },
-    slidesPerView: 'auto',
+    slidesPerView: "auto",
     pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true
+      el: ".swiper-pagination",
+      type: "bullets",
+      clickable: true,
     },
     breakpoints: {
       320: {
         slidesPerView: 1,
-        spaceBetween: 20
+        spaceBetween: 20,
       },
 
       1200: {
         slidesPerView: 3,
-        spaceBetween: 20
-      }
-    }
+        spaceBetween: 20,
+      },
+    },
   });
 
   /**
    * Animation on scroll
    */
-  window.addEventListener('load', () => {
+  window.addEventListener("load", () => {
     AOS.init({
       duration: 1000,
-      easing: 'ease-in-out',
+      easing: "ease-in-out",
       once: true,
-      mirror: false
-    })
+      mirror: false,
+    });
   });
 
-})()
+  /*Bard specific code for the toc and case study*/
+  const caseStudy = document.getElementById("case-study");
+  const caseStudyText = document.querySelector(".prose");
+  const toc = document.getElementById("toc");
+  const tocLinks = document.querySelectorAll("#toc a");
+  const tocLinksCount = tocLinks.length;
+
+  // Handlers
+  const handleScroll = () => {
+    handleTocVisibility();
+    handleTocSelection();
+  };
+
+  const handleResize = () => {
+    handleProseSize();
+    handleTocVisibility();
+    handleTocSelection();
+  };
+
+  const handlePageLoad = () => {
+    handleProseSize();
+    handleTocVisibility();
+    handleTocSelection();
+  };
+
+  const handleProseSize = () => {
+    if (window.innerWidth >= 1280) {
+      caseStudyText.classList.remove("prose-lg");
+      caseStudyText.classList.add("prose-xl");
+    } else if (window.innerWidth >= 1024) {
+      caseStudyText.classList.remove("prose-xl");
+      caseStudyText.classList.add("prose-lg");
+    } else {
+      caseStudyText.classList.remove("prose-lg");
+      caseStudyText.classList.remove("prose-xl");
+    }
+  };
+
+  const handleTocVisibility = () => {
+    console.log("case study top", caseStudy.offsetTop);
+    console.log("window scroll", window.scrollY);
+    console.log("window inner height", window.innerHeight);
+    console.log("offset height", caseStudy.offsetHeight);
+    const tocIsVisible = () => {
+      const caseStudyTopIsVisible = window.scrollY >= caseStudy.offsetTop;
+      const caseStudyBottomIsVisible =
+        window.scrollY + window.innerHeight >
+        caseStudy.offsetHeight + caseStudy.offsetTop;
+
+      return caseStudyTopIsVisible && !caseStudyBottomIsVisible;
+    };
+    console.log("is toc visible", tocIsVisible());
+    if (tocIsVisible()) {
+      toc.classList.add("show");
+    } else {
+      toc.classList.remove("show");
+    }
+  };
+
+  const handleTocSelection = () => {
+    const clearSelectedToc = () => {
+      const selectedTocItems = document.querySelectorAll("#toc .selected");
+      const subItems = document.querySelectorAll(`li.subitem.show`);
+
+      subItems.forEach((subItem) => {
+        subItem.classList.remove("show");
+      });
+
+      selectedTocItems.forEach((selectedTocItem) => {
+        selectedTocItem.classList.remove("selected");
+      });
+    };
+
+    const selectTocItem = (link) => {
+      const tocItem = link.closest("li");
+      const dataSection = tocItem.dataset.section;
+      const subItems = document.querySelectorAll(
+        `li.subitem[data-section="${dataSection}"]`
+      );
+
+      subItems.forEach((subItem) => {
+        subItem.classList.add("show");
+      });
+
+      tocItem.classList.add("selected");
+    };
+
+    if (!toc.classList.contains("show")) return;
+
+    for (let i = tocLinksCount - 1; i >= 0; i--) {
+      const link = tocLinks[i];
+      if (!link.hash) continue;
+      const target = document.querySelector(link.hash);
+
+      if (!!target && target.offsetTop <= window.scrollY + 16 * 2) {
+        clearSelectedToc();
+        selectTocItem(link, target);
+
+        break;
+      }
+    }
+  };
+
+  // Helpers
+  const throttle = (callback, wait) => {
+    let prevent = false;
+
+    return function () {
+      if (!prevent) {
+        callback();
+        prevent = true;
+        setTimeout(function () {
+          prevent = false;
+        }, wait);
+      }
+    };
+  };
+
+  //events
+  document.addEventListener("DOMContentLoaded", handlePageLoad);
+  document.addEventListener("scroll", throttle(handleScroll, 16));
+  window.addEventListener("resize", throttle(handleResize, 16));
+})();
